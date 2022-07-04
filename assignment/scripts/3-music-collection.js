d818933b7eeb05b73584b76bac6d6ecbf81516c6
Latisha Copeland <lcopeland001@gmail.com>
2022-07-04 13:49:40 -0500

    add function add to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,18 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection ( title, artist, yearPublished) {
+    //console.log('Add this to collection:', addToCollection);
+    const newReccord = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    };
+    collection.push(newReccord);
+    return newReccord;
+}
+
+addToCollection ('My Girl', 'Michael Jackson', 1986);
+
+// console.log(collection);
